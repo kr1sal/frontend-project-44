7139763b2271ea2127bbde862b5b3025d36d4636
kr1sal <kirillivanov523@gmail.com>
2024-10-22 00:22:49 +0300

    Add brain-calc.js and asciinema

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 [![Actions Status](https://github.com/kr1sal/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/kr1sal/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/8305d0b09061c0bb7272/maintainability)](https://codeclimate.com/github/kr1sal/frontend-project-44/maintainability)
 
-Demo: https://asciinema.org/a/2pILww2IyiozY6WEG7uIuBqO1
+Brain-even demo: https://asciinema.org/a/2pILww2IyiozY6WEG7uIuBqO1
+Brain-calc demo: https://asciinema.org/a/D8S5gklqiCGuz7Kd0Zxll5YWF
---
+++ bin/brain-calc.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import welcome from "../src/cli.js";
+
+const operations = [
+    {
+        operation: "+",
+        lambda: (a, b) => a + b,
+    },
+    {
+        operation: "-",
+        lambda: (a, b) => a - b,
+    },
+    {
+        operation: "*",
+        lambda: (a, b) => a * b,
+    },
+];
+
+const playBrainCalc = (userName = welcome()) => {
+    console.log("What is the result of the expression?")
+
+    for (let i = 0; i < 3; i += 1) {
+        const number1 = Math.floor(Math.random() * 100);
+        const number2 = Math.floor(Math.random() * 100);
+        const operationIndex = parseInt(Math.random() * 3);
+        const rightAnswer = operations[operationIndex].lambda(number1, number2)
+        console.log(`Question: ${number1} ${operations[operationIndex].operation} ${number2}`);
+        const answer = parseInt(readlineSync.question("Your answer: "));
+        if (rightAnswer === answer) {
+            console.log("Correct!");
+        }
+        else {
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+            console.log(`Let's try again, ${userName}!`);
+            return;
+        }
+    }
+
+    console.log(`Congratulations, ${userName}!`)
+}
+
+playBrainCalc();
