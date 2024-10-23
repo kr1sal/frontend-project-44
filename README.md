b24fea945d905095ac940f340bbc9cbc4ddbb168
kr1sal <kirillivanov523@gmail.com>
2024-10-23 23:50:51 +0300

    Add brain-gcd.js

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 [![Actions Status](https://github.com/kr1sal/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/kr1sal/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/8305d0b09061c0bb7272/maintainability)](https://codeclimate.com/github/kr1sal/frontend-project-44/maintainability)
 
-Brain-even demo: https://asciinema.org/a/2pILww2IyiozY6WEG7uIuBqO1
-Brain-calc demo: https://asciinema.org/a/D8S5gklqiCGuz7Kd0Zxll5YWF
+- Brain-even demo: https://asciinema.org/a/2pILww2IyiozY6WEG7uIuBqO1
+- Brain-calc demo: https://asciinema.org/a/D8S5gklqiCGuz7Kd0Zxll5YWF
+- Brain-gcd demo: https://asciinema.org/a/BhLIBZWYWtWKTZtIf7r5rlhFK
---
+++ bin/brain-gcd.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import welcome from "../src/cli.js";
+
+const gcd = (a, b) => {
+    if (a > b) {
+        if (a % b === 0) {
+            return b;
+        }
+        return gcd(a % b, b);
+    }
+    else {
+        if (b % a === 0) {
+            return a;
+        }
+        return gcd(a, b % a);
+    }
+}
+
+const playBrainGcd = (userName = welcome()) => {
+    console.log("Find the greatest common divisor of given numbers.")
+
+    for (let i = 0; i < 3; i += 1) {
+        const question = [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)];
+        const rightAnswer = gcd(...question);
+        console.log(`Question: ${question.join(" ")}`);
+        const answer = parseInt(readlineSync.question("Your answer: "));
+        if (rightAnswer === answer) {
+            console.log("Correct!");
+        }
+        else {
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+            console.log(`Let's try again, ${userName}!`);
+            return;
+        }
+    }
+
+    console.log(`Congratulations, ${userName}!`)
+}
+
+playBrainGcd()
